66a97ed769a7969c401f54fcf55e13de6277af78
RSM Harsha <postboxofharsha@gmail.com>
2026-01-07 01:28:28 +0530

    added db connection and password hashing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,19 +1,66 @@
-export const register = (req, res) => {
-    const { name, username, email, password } = req.body;
+import validator from 'validator';
+import { User } from "../models/User.js";
+import bcrypt from "bcryptjs"
 
-    if (!name || !username || !email || !password) {
-        return res.status(400).json({
-            message: "All fields are required",
-        })
-    }
+export const register = async (req, res) => {
 
-    if (password.length < 6) {
-        return res.status(400).json({
-            message: "Password must be at least 6 characters",
+    try {
+        let { name, username, email, password } = req.body;
+
+        if (!name || !username || !email || !password) {
+            return res.status(400).json({
+                message: "All fields are required",
+            })
+        }
+
+        name = name.trim()
+        email = email.trim()
+        username = username.trim()
+
+        if (!/^[a-zA-Z0-9_-]{1,20}$/.test(username)) {
+            return res.status(400).json({ message: 'Username must be 1–20 characters, using letters, numbers, _ or -.' })
+        }
+
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ message: 'Invalid email format' })
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({
+                message: "Password must be at least 6 characters",
+            });
+        }
+
+        //  Check if user already exists
+        const existingUser = await User.findOne({
+            $or: [{ email }, { username }],
+        });
+
+        if (existingUser) {
+            return res.status(409).json({
+                message: "User already exists",
+            });
+        }
+
+        password = await bcrypt.hash(password, 10)
+
+        // Save user (no hashing yet)
+        const user = await User.create({
+            name,
+            username,
+            email,
+            password,
+        });
+
+        return res.status(201).json({
+            message: "User registered successfully",
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Server error",
         });
     }
 
-    return res.status(201).json({
-        message: "User registered successfully",
-    });
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from "dotenv"
+
+import { connectDB } from "./config/db.js";
 import { authRouter } from './routes/auth.routes.js'
 
 
 const app = express()
+dotenv.config();
 
 app.use(cors({ origin: ["http://localhost:5500", "http://127.0.0.1:5500"] }))
 app.use(express.json())
+
+connectDB();
+
 app.use("/auth", authRouter)
 
 const PORT = 5000
